6c31658b76388a3e5230ac5b001e5077591d6838
QJesus <208780931@qq.com>
2016-01-01 01:16:00 +0800

    修改编辑内容

--- AliYunServer/Controllers/NoteController.cs
+++ AliYunServer/Controllers/NoteController.cs
@@ -38,25 +38,28 @@ namespace AliYunServer.Controllers
         {
             string template = Server.MapPath("~/shtml/template.shtml");
 
-            string html = null;
+            string html;
             using (Stream stream = System.IO.File.OpenRead(template))
             using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
             {
                 html = reader.ReadToEnd();
             }
 
-            var news = from ns in Db.News
-                       select ns;
+            int count = Db.News.Count();
             int index = 1;
-            foreach (News @new in news)
+            foreach (News @new in Db.News)
             {
-                html = html.Replace("@title", @new.Title)
-                            .Replace("@postdate", @new.PostDateTime.ToString(CultureInfo.InvariantCulture))
-                            .Replace("@article", @new.Article);
+                int last = Math.Max(1, index - 1);
+                int next = Math.Min(count, index + 1);
+                string temp = html.Replace("@title", @new.Title)
+                            .Replace("@postdate", @new.PostDateTime.ToString())
+                            .Replace("@article", @new.Article)
+                            .Replace("@last", last.ToString()).Replace("@next", next.ToString());
+                // ReSharper disable once AssignNullToNotNullAttribute
                 string file = Path.Combine(Path.GetDirectoryName(template), "heart", index + ".shtml");
                 using (Stream stream = System.IO.File.OpenWrite(file))
                 {
-                    byte[] buffer = Encoding.UTF8.GetBytes(html);
+                    byte[] buffer = Encoding.UTF8.GetBytes(temp);
                     stream.Write(buffer, 0, buffer.Length);
                 }
                 index++;
@@ -65,5 +68,35 @@ namespace AliYunServer.Controllers
             return Json(new { Msg = "搞定！" });
         }
 
+        [HttpGet]
+        public ActionResult Qpanpan(int id)
+        {
+            News news = Db.News.SingleOrDefault(ns => ns.ID == id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        [ValidateInput(false)]
+        [HttpPost]
+        public JsonResult Update(int id, string title, string note)
+        {
+            News news = Db.News.SingleOrDefault(ns => ns.ID == id);
+            if (news != null)
+            {
+                news.Title = title;
+                news.Article = note;
+
+                Db.Entry(news).State = EntityState.Modified;
+            }
+
+            int row = Db.SaveChanges();
+            return Json(new
+            {
+                Msg = (row > 0) ? "ok" : "error",
+            });
+        }
     }
 }
